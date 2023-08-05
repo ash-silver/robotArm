2f57d53806a6afbca17b534ef16965f0dceefc37
ash-silver <passlion@naver.com>
2023-08-05 22:35:35 +0900

    Initial commit

---
+++ src/main/java/com/example/robotarm/RobotArmApplication.java
@@ -0,0 +1,13 @@
+package com.example.robotarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotArmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RobotArmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/robotarm/RobotArmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.robotarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotArmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
